5c94db7f4951f79bb3e47c02e338350c639f69b1
Sashagm <example@example.com>
2023-07-12 01:17:25 +0500

    Добавлены PhpUnit tests

---
+++ tests/ExampleTest.php
@@ -0,0 +1,15 @@
+<?php
+
+
+namespace Sashagm\Money\Tests;
+
+use Sashagm\Money\Tests\TestCase;
+
+class ExampleTest extends TestCase
+{
+    /** @test  */
+    public function testExample()
+    {
+        $this->assertTrue(true);
+    }
+}
